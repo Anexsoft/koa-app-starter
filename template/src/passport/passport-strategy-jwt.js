9a5bb4b39f993d81e6cd7a33aee874d7ed0f57de
Kat Lim Ruiz <katlim@ieholding.com>
2019-02-13 11:02:46 -0500

    create method defaultOptions for readability

--- template/src/passport/passport-strategy-jwt.js
+++ template/src/passport/passport-strategy-jwt.js
@@ -1,11 +1,27 @@
+'use strict';
+
 const passport = require('koa-passport');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const _merge = require('lodash.merge');
 
-function setup(passport, options) {
+function setup(koaApp, passport, options) {
     // setup the strategy as jwt
-    options = _merge({
+    options = _merge(_defaultOptions(), options);
+
+    var jwtOptions = {
+        jwtFromRequest: options.extractJwtFrom,
+        issuer: options.whoIssuedTheToken,
+        secretOrKey: options.keyToEncryptTheToken,
+        audience: options.whoUsesTheToken
+    };
+
+    passport.use(options.strategyName, new JwtStrategy(jwtOptions, passport.passportVerifyDelegate));
+    koaApp.log.debug('passport-jwt-setup: success', jwtOptions);
+}
+
+function _defaultOptions() {
+    return {
         // set the strategy name in case we want duplicate strategies
         strategyName: 'jwt',
         // extraction mode (Authorization Bearer, Headers, etc)
@@ -16,20 +32,13 @@ function setup(passport, options) {
         keyToEncryptTheToken: null,
         // who was going to use the token
         whoUsesTheToken: null
-    }, options);
-
-    var jwtOptions = {
-        jwtFromRequest: options.extractJwtFrom,
-        issuer: options.whoIssuedTheToken,
-        secretOrKey: options.keyToEncryptTheToken,
-        audience: options.whoUsesTheToken
     };
-
-    passport.use(options.strategyName, new JwtStrategy(jwtOptions, passport.passportVerifyDelegate));
 }
 
 async function auth(ctx, next) {
     // https://github.com/rkusa/koa-passport/issues/125#issuecomment-462614317
+
+    // FIXME: strategy name should come from the setup
     return passport.authenticate('jwt', { session: false }, async(err, user, info) => {
         if (err || !user) {
             ctx.app.log.error('passport-auth: unauthorized', { err: err, user: user, info: info });
