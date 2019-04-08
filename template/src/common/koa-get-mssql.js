e681ace4c225b8ce15823f96424f2a7dcd072acf
katlimruiz <katlim_ruiz@yahoo.com>
2019-04-08 15:44:38 -0500

    adding mssql pool events to catch reset connection events which were supposed to be harmless

--- template/src/common/koa-get-mssql.js
+++ template/src/common/koa-get-mssql.js
@@ -10,26 +10,49 @@ function koaGetMsSql(koaApp, connectionObject, methodName) {
     var instName = '_' + methodName;
     var fnGetDb = async function() {
         if (!koaApp[instName]) {
-            try {
-                koaApp.log.debug(`mssql ${methodName}: creating pool`, connectionObject);
-                koaApp[instName] = new mssql.ConnectionPool(connectionObject);
-                koaApp.log.info(`mssql ${methodName}: pool created`);
-            } catch (err) {
-                koaApp.log.error(`mssql ${methodName}: connection error`, err);
-                throw err;
-            }
+            koaApp.log.debug(`mssql ${methodName}: pool creating...`, connectionObject);
+            koaApp[instName] = new mssql.ConnectionPool(connectionObject);
+            koaApp.log.info(`mssql ${methodName}: pool created`);
+
+            koaApp[instName].on('error', err => {
+                switch (err.message) {
+                // NOTE: these are known errors when the connections simply go idle, in theory mssql.ConnectionPool should handle
+                // reconnecting and expanding the pool when necessary.
+                // https://github.com/tediousjs/node-mssql/issues/568
+                case 'Connection lost - write ECONNRESET':
+                case 'Connection lost - read ECONNRESET':
+                case 'Connection lost - socket hang up':
+                    koaApp.log.debug(`mssql ${methodName}: pool connection reset`, err);
+                    break;
+                default:
+                    koaApp.log.error(`mssql ${methodName}: pool event error`, err);
+                }
+            });
         }
 
         if (!koaApp[instName].connected) {
-            await koaApp[instName].connect();
-            koaApp.log.info(`mssql ${methodName}: pool connected`);
+            try {
+                await koaApp[instName].connect();
+                koaApp.log.info(`mssql ${methodName}: pool connect ok`);
+            } catch (err) {
+                koaApp.log.error(`mssql ${methodName}: pool connect error`, err);
+                throw err;
+            }
         }
 
         return koaApp[instName];
     };
 
+    mssql.on('connect', err => {
+        if (err) {
+            koaApp.log.error(`mssql ${methodName}: connect event error`, err);
+        } else {
+            koaApp.log.info(`mssql ${methodName}: connect event ok`);
+        }
+    });
+
     mssql.on('error', err => {
-        koaApp.log.error(`mssql ${methodName}: error`, err);
+        koaApp.log.error(`mssql ${methodName}: error event`, err);
     });
 
     koaApp[methodName] = fnGetDb;
