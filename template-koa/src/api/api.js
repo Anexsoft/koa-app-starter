433d181023381002e02bdcb3777386a707c78574
katlim@ieholding.com <katlim@ieholding.com>
2020-01-21 00:39:22 -0500

    moving passport configuration to init (pending refactor this)

--- template-koa/src/api/api.js
+++ template-koa/src/api/api.js
@@ -1,10 +1,6 @@
 'use strict';
 
 const KoaRouter = require('koa-router');
-const passportSetup = require('../passport/passport-adapter').setup;
-const passportJwtSetup = require('../passport/passport-strategy-jwt').setup;
-const passportJwtAuthenticate = require('../passport/passport-strategy-jwt').authenticate;
-const passportJwtAuthorizeMw = require('../passport/passport-strategy-jwt').buildAuthorizeMw;
 
 /**
  * Setup the api (config and routes)
@@ -12,37 +8,12 @@ const passportJwtAuthorizeMw = require('../passport/passport-strategy-jwt').buil
  * @param {*} config Configuration
  */
 async function setup(koaApp) {
-    // setup the authentication
-    _setupAuth(koaApp);
-
     // setup the routes
     _setupRoutes(koaApp);
-
-    // setup the db layer
-    // db.setup(koaApp, config);
-}
-
-function _setupAuth(koaApp) {
-    // set the passport basis
-    var passport = passportSetup(koaApp, (payload) => {
-        // TODO: change function to convert from payload to user object
-        return payload; 
-    });
-
-    // set the strategy
-    var jwtcfg = koaApp.cfg.auth.jwt;
-    passportJwtSetup(koaApp, passport, {
-        whoIssuedTheToken: jwtcfg.whoIssuedTheToken,
-        keyToEncryptTheToken: jwtcfg.keyToEncryptTheToken,
-        whoUsesTheToken: jwtcfg.whoUsesTheToken
-    });
-
-    koaApp.log.debug('api-auth: success');
 }
 
 function _setupRoutes(koaApp) {
     var koaRouter = new KoaRouter();
-    koaApp.router = koaRouter;
 
     // TODO: setup routes
     __setupRoutes(koaApp, koaRouter);
@@ -70,7 +41,7 @@ function __setupRoutes(koaApp, koaRouter) {
 
     // protected endpoint + user, note that 'passportJwtAuthenticate' is used as a pre-step to the real action
     koaRouter
-        .get('/xxx2/:id', passportJwtAuthenticate, async function(ctx, next) {
+        .get('/xxx2/:id', koaApp.passportUtil.authenticate, async function(ctx, next) {
             ctx.log.debug('getXXX2ById-api: start', ctx.params);
             ctx.body = {
                 id: ctx.params['id'],
@@ -82,7 +53,7 @@ function __setupRoutes(koaApp, koaRouter) {
 
     // protected endpoint + roles, validate the user and also which roles that user has
     koaRouter
-        .get('/xxx3/:id', passportJwtAuthenticate, passportJwtAuthorizeMw(['admin']), async function(ctx, next) {
+        .get('/xxx3/:id', koaApp.passportUtil.authenticate, koaApp.passportUtil.generateValidateRoles(['admin']), async function(ctx, next) {
             ctx.log.debug('getXXX3ById-api: start', ctx.params);
             ctx.body = {
                 id: ctx.params['id'],
--- template-koa/src/init.js
+++ template-koa/src/init.js
@@ -4,10 +4,16 @@ const path = require('path');
 const fs = require('fs-extra');
 const _merge = require('lodash.merge');
 const koaHealthProbe = require('@juntoz/koa-health-probe');
+const passport = require('koa-passport');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
 
 async function init(koaApp, moduleEntryPath) {
     // pre-processing
-    _pre(koaApp);
+    await _pre(koaApp);
+
+    // auth
+    await _initAuth(koaApp);
 
     // api
     await _initApi(koaApp, moduleEntryPath);
@@ -16,7 +22,12 @@ async function init(koaApp, moduleEntryPath) {
     _post(koaApp);
 }
 
-function _pre(koaApp) {
+async function _pre(koaApp) {
+    // load the api config
+    var configPath = path.join(__dirname, 'config.json');
+    var loadedCfg = await fs.readJson(configPath);
+    koaApp.cfg = _merge(_defaultConfig(), loadedCfg);
+
     // set request id
     const xRequestId = require('koa-x-request-id');
     koaApp.use(xRequestId({ noHyphen: true, inject: true }, koaApp));
@@ -33,6 +44,97 @@ function _pre(koaApp) {
     koaApp.log.debug('app-bodyparser: success');
 }
 
+async function _initAuth(koaApp) {
+    if (koaApp.cfg.auth) {
+        // setup passport (to authenticate requests)
+        koaApp.use(passport.initialize());
+        koaApp.log.debug('passport-setup: passport init success');
+
+        if (!koaApp.cfg.auth.jwt) {
+            throw new Error('passport only supported method is jwt for now');
+        }
+
+        var jwtOptions = {
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            issuer: koaApp.cfg.auth.jwt.issuer,
+            secretOrKey: koaApp.cfg.auth.jwt.secretOrKey,
+            audience: koaApp.cfg.auth.jwt.audience
+        };
+
+        passport.use('jwt', new JwtStrategy(jwtOptions, _passportVerify));
+        koaApp.passportUtil = {
+            authenticate: _authenticate,
+            generateValidateRoles: _mwValidateRoles
+        };
+        koaApp.log.debug('passport-setup: jwt success', jwtOptions);
+    } else {
+        koaApp.log.info('passport-setup: anonymous access');
+    }
+}
+
+function _passportVerify(payload, doneCallback) {
+    // verify the payload and convert to the user object which becomes the identity
+    // of the request.
+    try {
+        // var user = fnPayloadToUser(payload);
+        var user = payload;
+        if (user) {
+            koaApp.log.debug('passport-verify: success', user);
+            return doneCallback(null, user);
+        } else {
+            koaApp.log.error('passport-verify: false');
+            return doneCallback(null, false);
+        }
+    } catch (error) {
+        koaApp.log.error('passport-verify: error');
+        return doneCallback(error, false);
+    }
+}
+
+async function _authenticate(ctx, next) {
+    // https://github.com/rkusa/koa-passport/issues/125#issuecomment-462614317
+
+    return passport.authenticate('jwt', { session: false }, async(err, user, info) => {
+        if (err || !user) {
+            ctx.log.error('passport-auth: user unauthenticated', { err: err, user: user, info: info });
+            ctx.throw(401, 'passport-auth: user unauthenticated');
+        }
+
+        ctx.log.debug('passport-auth: user authenticated', { user: user });
+
+        // NOTE: in theory passport should do this automatically, although I haven't found a way to do it
+        // The good part is by assigning this, ctx.isAuthenticated gives you true as expected
+        ctx.req.user = user;
+
+        await next();
+    })(ctx);
+};
+
+function _mwValidateRoles(acceptRoles) {
+    if (!acceptRoles) {
+        acceptRoles = [];
+    }
+
+    if (typeof acceptRoles === 'string') {
+        acceptRoles = [acceptRoles];
+    }
+
+    return async(ctx, next) => {
+        if (acceptRoles.length > 0 && ctx.req.user && ctx.req.user.roles) {
+            var rolesMatch = ctx.req.user.roles.filter(r => acceptRoles.indexOf(r) >= 0);
+            if (rolesMatch.length === 0) {
+                ctx.log.error('passport-auth: user unauthorized', { user: ctx.req.user, expected: acceptRoles });
+                ctx.throw(401, 'passport-auth: user unauthorized');
+            } else {
+                ctx.log.debug('passport-auth: user authorized', { user: ctx.req.user, expected: acceptRoles });
+            }
+        }
+
+        // if no roles are needed, just bypass
+        await next();
+    };
+};
+
 async function _initApi(koaApp, modulePath) {
     // load the api module from the index root
     const moduleSetup = require.main.require(modulePath);
@@ -41,11 +143,6 @@ async function _initApi(koaApp, modulePath) {
     } else {
         koaApp.log.debug(`app-api: ${modulePath} found`);
     }
-
-    // load the api config
-    var configPath = path.join(__dirname, 'config.json');
-    var loadedCfg = await fs.readJson(configPath);
-    koaApp.cfg = _merge(_defaultConfig(), loadedCfg);
 
     // call the api setup
     await moduleSetup(koaApp);
@@ -57,9 +154,9 @@ function _defaultConfig() {
         name: null,
         auth: {
             jwt: {
-                whoIssuedTheToken: null,
-                keyToEncryptTheToken: null,
-                whoUsesTheToken: null            
+                issuer: null,
+                secretOrKey: null,
+                audience: null            
             }
         }
     };
