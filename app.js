8bc023f99c24730c55b9486038fb2b61c60b9cd2
katlim@ieholding.com <katlim@ieholding.com>
2020-02-01 00:18:34 -0500

    create .starter folder to store last answers, and last execution info

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const path = require('path');
 const execSync = require('child_process').execSync;
 const Plugin = require('./Plugin');
 const ReplaceVarsTask = require('./tasks/ReplaceVarsTask');
-const fs = require('fs-extra');
-const jsyaml = require('js-yaml');
 
 async function run(options) {
     console.log('> Starting');
@@ -41,10 +39,6 @@ async function run(options) {
             devDeps.push(...pdeps.devDependencies);
         }
     }
-
-    // write a file with the koa-app-starter version that was lastly used. This way we can track back which one was used and if the app is too old.
-    console.log('> Drop version file');
-    await writeLastTraceFile(options);
 
     // run npm completely (always set as the last step)
     await _npmInstall(deps, devDeps);
@@ -109,33 +103,6 @@ async function _replaceVariablesInFiles(sourcePath, vars) {
     }
 
     console.log(`> Done updated vars`);
-}
-
-async function writeLastTraceFile(options) {
-    var answers = Object.assign({}, options.answers);
-    
-    // clear some answers that we do need to ask every time
-    delete answers.continueWithoutPkgJson;
-    delete answers.go; 
-
-    var trace = {
-        ts: new Date().toISOString(),
-        user: require("os").userInfo().username,
-        version: options.appVersion,
-        answers: answers
-    };    
-
-    var headerdoc = `
-## This file was created to trace back what is the last version of @juntoz/koa-app-starter that updated this application.
-## and to reload the answers when the starter is ran again.
-## It is safe to delete or change. It is not used to run the application.
-    `;
-
-    var ymltxt = 
-        headerdoc + '\n' + 
-        jsyaml.safeDump(trace);
-
-    await fs.writeFile(path.join(options.dest, 'starterlast.yml'), ymltxt);
 }
 
 module.exports = run;
--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const inquirer = require('inquirer');
 const process = require('process');
 const path = require('path');
 const fs = require('fs-extra');
+const jsyaml = require('js-yaml');
 
 const run = require('./app');
+
+var starterOutputPath = null; 
 
 async function doInit(initData) {
     var root = process.cwd();
@@ -14,14 +17,30 @@ async function doInit(initData) {
     // screw up this app, therefore append a dist folder so it will not interfere
     if (root.indexOf('koa-app-starter') >= 0) {
         root = path.resolve(root, '_test');
-        console.info('Warning: you are located in the koa-app-starter root folder\n');
+        console.info('Warning: you are located in the koa-app-starter root folder');
     }
 
-    var answers = await inquirer.prompt(_questions(root));
+    starterOutputPath = path.join(root, '.starter');
+
+    var lastAnswers = await loadYamlToJson(path.join(starterOutputPath, 'answers.yml'));
+    if (!lastAnswers) {
+        console.debug(`Note: No answers file found.`);
+    }
+
+    console.log('\n');
+    var answers = await inquirer.prompt(_questions(root, lastAnswers));
     if (answers.go) {
         answers.fullappname = ns_plus_domain(answers.appns, answers.appname);
+
+        // save the current runtime
+        await saveRuntime(initData);
+
+        // save the answers first
+        await saveAnswers(answers);
+
+        // execute
         await run({
-            appVersion: initData.appVersion,
+            
             dest: root,
             answers: answers
         });
@@ -32,10 +51,14 @@ function ns_plus_domain(ns, dom) {
     return ns + '-' + dom;
 }
 
-function _questions(root) {
+function _questions(root, lastAnswers) {
+    lastAnswers = lastAnswers || {};
+
+    // validator for namespaces and application name
     var _minLettersAndHyphen = '^[a-z]{2,20}(-[a-z]{2,20})###$';
     var minTwoWordsLettersAndHyphen = new RegExp(_minLettersAndHyphen.replace('###', '+'));
     var minOneWordLettersAndHyphen = new RegExp(_minLettersAndHyphen.replace('###', '*'));
+    
     var defaultPort = 3000;
 
     return [
@@ -43,13 +66,14 @@ function _questions(root) {
             type: 'list',
             message: 'APP: What type should this app be?',
             name: 'apptype',
-            default: 'koa-api',
+            default: lastAnswers.apptype || 'koa-api',
             choices: ['koa-api']
         },
         {
             type: 'input',
             message: 'AUTH: What audience should this app belong to?',
             name: 'appaudience',
+            default: lastAnswers.appaudience || null,
             when: (ans) => ans.apptype == 'koa-api',
             validate: (input, ans) => {
                 if (input !== '' && minTwoWordsLettersAndHyphen.test(input)) {
@@ -63,12 +87,13 @@ function _questions(root) {
             type: 'confirm',
             message: 'DB: Does this app need to connect to mssql?',
             name: 'addmssql',
-            default: false,
+            default: lastAnswers.addmssql || false,
         },
         {
             type: 'input',
             message: 'K8S: What should be the app namespace in kubernetes?',
             name: 'appns',
+            default: lastAnswers.appns || null,
             validate: (input, ans) => {
                 if (minTwoWordsLettersAndHyphen.test(input)) {
                     return true;
@@ -81,6 +106,7 @@ function _questions(root) {
             type: 'input',
             message: (ans) => `K8S: What should be the name of your docker image?`,
             name: 'appname',
+            default: lastAnswers.appname || null,
             transformer: (input, ans, opt) => ns_plus_domain(ans.appns, input),
             validate: (input, ans) => {
                 if (input !== '' && minOneWordLettersAndHyphen.test(input)) {
@@ -94,7 +120,7 @@ function _questions(root) {
             type: 'number',
             message: 'NET: Which port should this app listen to?',
             name: 'appport',
-            default: defaultPort,
+            default: lastAnswers.appport || defaultPort,
             when: (ans) => ans.apptype == 'koa-api',
             validate: (input, ans) => {
                 if (Number.isInteger(input) && input >= defaultPort) {
@@ -124,11 +150,60 @@ function _questions(root) {
     ];
 }
 
+async function saveRuntime(initData) {
+    initData.ts = new Date().toISOString();
+
+    var headerdoc = `
+## This file was created to timestamp the last execution to know which starter version was used in the application.
+## It is safe to delete. It is not mandatory to have it to run the starter.
+    `;
+
+    var ymltxt = 
+        headerdoc + '\n' + 
+        jsyaml.safeDump(initData);
+
+    await saveYaml(path.join(starterOutputPath, 'last.yml'), ymltxt);
+}
+
+async function saveAnswers(answers) {
+    var toSave = Object.assign({}, answers);
+    
+    // clear some answers that we do need to ask every time
+    delete toSave.continueWithoutPkgJson;
+    delete toSave.go; 
+
+    var headerdoc = `
+## This file was created to store the last answers that were put so we can reuse them when a new version needs to be updated.
+## It is safe to delete. It is not mandatory to have it to run the starter.
+    `;
+
+    var ymltxt = 
+        headerdoc + '\n' + 
+        jsyaml.safeDump(toSave);
+
+    await saveYaml(path.join(starterOutputPath, 'answers.yml'), ymltxt);
+}
+
+async function loadYamlToJson(srcFile) {
+    try {
+        var inputContent = await fs.readFile(srcFile);
+        return jsyaml.safeLoad(inputContent);
+    } catch (error) {
+        return null;
+    }
+}
+
+async function saveYaml(destFile, yamlContent) {
+    await fs.ensureFile(destFile);
+    await fs.writeFile(destFile, yamlContent);
+}
+
 (async () => {
     const boxen = require('boxen');
     const { version } = require('./package.json');
     console.log(boxen('KOA-APP-STARTER v' + version, { padding: 1 }));
     await doInit({
-        appVersion: version
+        appVersion: version,
+        user: require("os").userInfo().username,
     });
 })();
