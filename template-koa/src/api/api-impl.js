14180f1e5a631138653251607bea2ce539115b62
Kat Lim <katlim@ieholding.com>
2020-02-13 15:14:25 -0500

    add error handler to api impl as example for all endpoints

--- template-koa/src/api/api-impl.js
+++ template-koa/src/api/api-impl.js
@@ -4,25 +4,49 @@ async function setup(koaApp) {
     // TODO: fill with setup logic
 }
 
+function _handleAppErrorOrThrow(ctx, error) {
+    if (error.name == 'AppError') {
+        if (error.errorCode == 'ERR_GET_ID') {
+            ctx.throw(404);
+        } else {
+            ctx.throw(400, error.toString());
+        }
+    } else {
+        throw error;
+    }
+}
+
 async function xxx1(ctx, next) {
-    ctx.body = AR.ok({
-        id: ctx.params.id,
-        name: 'My id1 is ' + ctx.params.id
-    });
+    try {
+        ctx.body = AR.ok({
+            id: ctx.params.id,
+            name: 'My id1 is ' + ctx.params.id
+        });
+    } catch (error) {
+        _handleAppErrorOrThrow(ctx, error);
+    }
 }
 
 async function xxx2(ctx, next) {
-    ctx.body = AR.ok({
-        id: ctx.params.id,
-        name: 'Protected ' + ctx.params.id
-    });
+    try {
+        ctx.body = AR.ok({
+            id: ctx.params.id,
+            name: 'Protected ' + ctx.params.id
+        });
+    } catch (error) {
+        _handleAppErrorOrThrow(ctx, error);
+    }
 }
 
 async function xxx3(ctx, next) {
-    ctx.body = AR.ok({
-        id: ctx.params.id,
-        name: 'Protected and Role ' + ctx.params.id
-    });
+    try {
+        ctx.body = AR.ok({
+            id: ctx.params.id,
+            name: 'Protected and Role ' + ctx.params.id
+        });
+    } catch (error) {
+        _handleAppErrorOrThrow(ctx, error);
+    }
 }
 
 module.exports = {
