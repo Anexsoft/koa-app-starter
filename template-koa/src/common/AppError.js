23c7ea5d81b81664f8e510673efc24a97f20eed5
katlim@ieholding.com <katlim@ieholding.com>
2020-02-23 12:35:55 -0500

    leave AppError as errorCode:message

--- template-koa/src/common/AppError.js
+++ template-koa/src/common/AppError.js
@@ -1,18 +1,17 @@
 // NOTE: If this file is moved, verify the links to this file (especially in the partials).
 
 class AppError extends Error {
-    constructor(errorCode, message, statusCode) {
+    constructor(errorCode, message) {
         super(message);
 
         // Ensure the name of this error is the same as the class name
         this.name = this.constructor.name;
-
+        
         this.errorCode = errorCode;
-        this.statusCode = statusCode;
     }
 
     toString() {
-        return `${this.errorCode || this.name}:${this.message}` + (this.statusCode ? ':' + this.statusCode : '');
+        return `${this.errorCode || this.name}:${this.message}`;
     }
 }
 
