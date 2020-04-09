e1b2322d9d40ecb233f2fe457aca24ac53a2236e
pedrodlc1210 <delascasas.pedro@gmail.com>
2020-04-08 21:48:24 -0500

    fixing AppError.js

--- template-koa/src/common/AppError.js
+++ template-koa/src/common/AppError.js
@@ -6,12 +6,16 @@ class AppError extends Error {
 
         // Ensure the name of this error is the same as the class name
         this.name = this.constructor.name;
-        
+
         this.errorCode = errorCode;
     }
 
     toString() {
-        return `${this.errorCode || this.name}:${this.message}`;
+        if (this.message) {
+            return `${this.errorCode || this.name}:${this.message}`;
+        } else {
+            return `${this.errorCode || this.name}`;
+        }
     }
 }
 
