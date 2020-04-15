c9de22414e872c86e114d393028648de0ce6bc11
katlim@ieholding.com <katlim@ieholding.com>
2020-04-15 09:29:01 -0500

    AppError: errorCode and message is mandatory

--- template-koa/src/common/AppError.js
+++ template-koa/src/common/AppError.js
@@ -2,16 +2,21 @@
 
 class AppError extends Error {
     constructor(errorCode, message) {
+        if (!errorCode || !message) {
+            throw new Error('errorCode and message are mandatory');
+        }
+
         super(message);
 
         // Ensure the name of this error is the same as the class name
         this.name = this.constructor.name;
-        
+
         this.errorCode = errorCode;
     }
 
     toString() {
-        return `${this.errorCode || this.name}:${this.message}`;
+        var title = this.errorCode || this.name;
+        return `${title}:${this.message}`;
     }
 }
 
